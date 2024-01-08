4e12ff4e0226f83a7bf8163ac9bfe796787711cf
Salim Hossain <salim.hussain@htbbd.com>
2024-01-08 22:40:37 +0600

    DB Connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
 /** @format */
 
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
