2897975c494425d65ca2d86b9d037ccc8a687f74
Salim Hossain <salim.hussain@htbbd.com>
2024-01-16 02:42:42 +0600

    complete

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import path from "path";
 import userRouter from "../backend/routes/userRoute.js";
 import authRouter from "../backend/routes/authRoute.js";
 dotenv.config();
@@ -18,7 +19,18 @@ mongoose
     console.log(err);
   });
 
+//=> path
+const __dirname = path.resolve();
+
 const app = express();
+
+//=>
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+//=>
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cookieParser());
